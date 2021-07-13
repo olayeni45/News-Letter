c5a38c22fc46f2a4012819df4893e4352d0e3d0e
olayeni45 <olayeni45@gmail.com>
2021-07-13 23:18:38 +0100

    Ready to deploy

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post('/failure', (req, res) => {
     res.redirect('/');
 })
 
-app.listen(3000, () => {
-    console.log("Server listening at port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started");
 })
 
